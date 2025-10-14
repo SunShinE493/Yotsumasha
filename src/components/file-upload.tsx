c3b14c56d4849dbe63ffb403a86fd40501bb3b68
SunShinE493 <dayaojiangong@gmail.com>
2025-10-14 14:03:45 +0000

    Runei

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -81,7 +81,7 @@ import chemistry from "./data/chemistry.json";
 const availableJsonFiles = {
   "koumin.json": koumin,
   "koumin2.json": koumin2,
-  "runri.json": rinri,
+  "ruiri.json": rinri,
   "seikei.json": seikei,
   "chiri.json":chiri,
   "chiri2.json":chiri2,
